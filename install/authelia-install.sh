7c6e7795c5c6aa86fc2022d5678b7604edf15e0d
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2024-11-06 20:24:33 +0100

    test

--- install/authelia-install.sh
+++ install/authelia-install.sh
@@ -14,37 +14,8 @@ network_check
 update_os
 
 msg_info "Installing Dependencies (Patience)"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y wget
-$STD apt-get install -y gnupg
-$STD apt-get install -y build-essential
-$STD apt-get install -y libsqlite3-dev
+$STD apt-get install -y authelia
 msg_ok "Installed Dependencies"
-
-msg_info "Installing Golang"
-set +o pipefail
-RELEASE=$(curl -s https://go.dev/dl/ | grep -o "go.*\linux-amd64.tar.gz" | head -n 1)
-wget -q https://golang.org/dl/$RELEASE
-tar -xzf $RELEASE -C /usr/local
-$STD ln -s /usr/local/go/bin/go /usr/local/bin/go
-set -o pipefail
-msg_ok "Installed Golang"
-
-
-msg_info "Creating Service"
-service_path="/etc/systemd/system/authelia.service"
-echo "[Unit]
-Description=Authelia
-After=network-online.target
-[Service]
-User=root
-WorkingDirectory=/opt/authelia
-ExecStart=/opt/authelia/bin/kc.sh start-dev
-[Install]
-WantedBy=multi-user.target" >$service_path
-$STD systemctl enable --now authelia.service
-msg_ok "Created Service"
 
 motd_ssh
 customize
