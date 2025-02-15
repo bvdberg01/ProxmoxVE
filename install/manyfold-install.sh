3717fd9c2db5d234415fa63ec452def51a4490a5
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2025-02-15 19:13:13 +0100

    Update manyfold-install.sh

--- install/manyfold-install.sh
+++ install/manyfold-install.sh
@@ -54,7 +54,7 @@ msg_ok "Installed Node.js/Yarn"
 
 msg_info "Installing Ruby Version Manager"
 $STD gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable 
+curl -fsSL https://get.rvm.io | bash -s stable 
 source /usr/local/rvm/scripts/rvm
 msg_ok "Installed Ruby Version Manager"
 
