1d17f3b7d0d6bd2f65bc93e423468af44afbaef9
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2025-02-16 17:18:41 +0100

    Update sqlserver2022-install.sh

--- install/sqlserver2022-install.sh
+++ install/sqlserver2022-install.sh
@@ -23,13 +23,11 @@ $STD apt install -y \
 msg_ok "Installed Dependencies"
 
 msg_info "Setup SQL Server 2022"
-$STD curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-$STD curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
-#$STD curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
-#$STD curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | tee /etc/apt/sources.list.d/mssql-server-2022.list
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
 $STD apt-get clean *
 $STD apt-get update -y
-$STD apt-get install -y mssql-server
+apt-get install -y mssql-server
 msg_ok "Setup Server 2022"
 
 msg_info "Installing SQL Server Tools"
